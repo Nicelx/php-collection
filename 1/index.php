72beee67f0522d8d7bc72ab5ef5e152245f0c5ba
Nicelx <relush321@gmail.com>
2024-04-20 23:55:22 +0300

    form handler

--- 1/index.php
+++ 1/index.php
@@ -27,7 +27,30 @@
 		$namesss = ["Daniel2","Bella2","Frida2"];
 		
 		//Object type
-		$object = null //initial
+		$object = null; //initial
+
+		// superglobals
+		echo $_SERVER["DOCUMENT_ROOT"];
+		echo "<br>";
+		echo $_SERVER["PHP_SELF"];
+		echo "<br>";
+		echo $_SERVER["SERVER_NAME"];
+		echo "<br>";
+		echo $_SERVER["REQUEST_METHOD"];
+		
+		echo $_GET["name"];
+		
+		echo "<br>";
+		echo $_REQUEST["name"];
+		echo "<br>";
+		echo $_FILES["name"];
+		echo $_COOKIE["name"];
+		echo "<br>";
+		$_SESSION["username"] = "Krossing";
+		echo $_SESSION["username"];
+		echo "<br>";
+		$_ENV[""];
+
 	?>
 	<p>hi <?php echo $string ?></p>
 </body>
