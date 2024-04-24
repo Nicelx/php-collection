2aa2686882c2caf81ed274268004031707823d3c
Nicelx <relush321@gmail.com>
2024-04-24 22:53:36 +0300

    build in fun

--- 2/index.php
+++ 2/index.php
@@ -25,6 +25,26 @@
 		echo pow(3,2) . "<br>";
 		echo sqrt(9) . "<br>";
 		echo rand(1, 100) . "<br>";
+		
+		$array = ["apple", "banana", "orange"];
+		$array2 = ["car", "bicycle", "boat"];
+		echo is_array($array) . "<br>";
+		array_push($array, "kiwi");
+		print_r($array);
+		array_pop($array);
+		print_r($array);
+		echo '<br>';
+		print_r(array_reverse($array));
+		print_r(array_merge($array, $array2));
+		echo '<br>'; 	
+
+		$date = "2023-04-20 12:22:33";
+		echo date("Y-m-d H:i:s") . "<br>";
+		echo time() . "<br>";
+		echo strtotime($date) . "<br>";
+		
+
+
 	?>
 </body>
 </html>
