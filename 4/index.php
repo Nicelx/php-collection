4e70d0b9621058363c90e65f9f933d3b50d2fa91
Nicelx <relush321@gmail.com>
2024-05-02 23:56:58 +0300

    start formhandler

--- 4/index.php
+++ 4/index.php
@@ -6,6 +6,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 </head>
+<h3>Signup</h3>
+
+<form action="includes/formhandler.inc.php" method="post">
+	<input type="text" name="username" placeholder="Username">
+	<input type="password" name="pwd" placeholder="Password">
+	<input type="text" name="email" placeholder="E-mail">
+	<button>Signup</button>
+</form>
 
 <body>
 
